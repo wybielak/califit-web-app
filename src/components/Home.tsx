e8220de5fee840a912ac8cf7cb25293ac45b70f9
Dawid <31769799+wybielak@users.noreply.github.com>
2024-10-14 00:09:10 +0200

    pobieranie

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,22 @@
 import { observer } from "mobx-react-lite";
 import BottomMenu from "./BottomMenu";
 import Header from "./Header";
+import { useStore } from "../mobx/Store";
+import { useEffect, useState } from "react";
 
 
 export default observer(function Home() {
+
+  const { appStorage } = useStore()
+
+  const [load, setLoad] = useState(true)
+
+  useEffect(() => {
+    appStorage.getExercises().then(() => {
+      setLoad(false)
+    })
+  }, [])
+
   return (
     <>
       <Header />
--- src/mobx/AppStorage.ts
+++ src/mobx/AppStorage.ts
@@ -1,7 +1,15 @@
 import { signInWithPopup, signOut } from "firebase/auth";
 import { makeAutoObservable } from "mobx";
-import { auth, googleProvider } from "../config/FirebaseConfig";
+import { auth, db, googleProvider } from "../config/FirebaseConfig";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
+type Texercise = {
+    id: string;
+    name: string;
+    count: number;
+    goal: number;
+    unit: string;
+}
 
 export default class AppStorage {
     constructor() {
@@ -29,4 +37,27 @@ export default class AppStorage {
     }
 
     //SECTION - przechowywanie danych
+
+    workoutRef = collection(db, 'workout')
+
+    exercises = <Texercise[]>[]
+
+    setExercises = (data: Texercise[]) => {
+        this.exercises = data
+    }
+
+    getExercises = async () => {
+        const myExercisesRef = query(this.workoutRef, where('userId', '==', `${auth!.currentUser!.uid}`))
+
+        const data = await getDocs(myExercisesRef)
+        const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id } as Texercise))
+
+
+        this.setExercises(filteredData)
+
+        console.log('Pobrano cwiczenia')
+
+        console.log(this.exercises[0].name)
+
+    }
 }
