9a3a841a3a851f0ab7465a3d48b7ba761a337724
Dawid <31769799+wybielak@users.noreply.github.com>
2024-10-13 23:38:53 +0200

    test

--- src/config/FirebaseConfig.ts
+++ src/config/FirebaseConfig.ts
@@ -6,13 +6,7 @@ import { getFirestore } from "firebase/firestore"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBldy6f-JdRo93SLGR0Jr_dOPGmiZFK0b8",
-  authDomain: "califit-a4835.firebaseapp.com",
-  projectId: "califit-a4835",
-  storageBucket: "califit-a4835.appspot.com",
-  messagingSenderId: "425431825055",
-  appId: "1:425431825055:web:76e8add59e49805e8400aa",
-  measurementId: "G-CT4N51B050"
+  // Paste your API key
 };
 
 // Initialize Firebase
